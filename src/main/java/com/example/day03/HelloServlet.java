cee8bcd90a5c6ad664a4aeb781aedf585147e234
supunj506 <supunj506@gmail.com>
2023-07-12 05:05:25 +0530

    first commit

---
+++ src/main/java/com/example/day03/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.example.day03;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("hello servlet mapping ");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ src/main/java/com/example/day03/SaveStudent.java
@@ -0,0 +1,6 @@
+package com.example.day03;
+
+import javax.servlet.http.HttpServlet;
+
+public class SaveStudent extends HttpServlet{
+}
