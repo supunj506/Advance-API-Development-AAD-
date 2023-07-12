42db9a42e5044b712a79ccf7102b0767d2ac2cab
supunj506 <supunj506@gmail.com>
2023-07-12 05:41:58 +0530

    map SaveStudent class

--- src/main/java/com/example/day03/SaveStudent.java
+++ src/main/java/com/example/day03/SaveStudent.java
@@ -1,6 +1,22 @@
 package com.example.day03;
 
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SaveStudent extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("SaveStudent Post Work");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
 }
