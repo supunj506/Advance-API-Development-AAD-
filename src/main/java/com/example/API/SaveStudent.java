d90358dfa4975e556b01af2e741e075ee5a0e9fe
supunj506 <supunj506@gmail.com>
2023-07-12 11:10:53 +0530

    clean code

--- src/main/java/com/example/API/SaveStudent.java
+++ src/main/java/com/example/API/SaveStudent.java
@@ -1,11 +1,9 @@
-package com.example.day03;
+package com.example.API;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
