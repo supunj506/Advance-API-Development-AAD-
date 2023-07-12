550cd56e10b6f98134cc205a03c2815a4cc8675c
supunj506 <supunj506@gmail.com>
2023-07-12 08:39:52 +0530

    save data database using init parem method

--- src/main/java/com/example/day03/SaveStudent.java
+++ src/main/java/com/example/day03/SaveStudent.java
@@ -5,13 +5,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class SaveStudent extends HttpServlet{
 
+
+
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String queryString = req.getQueryString();
-        System.out.println(queryString);
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+
+        System.out.println(id+"\n"+name+"\n"+address);
+
+
+        try {
+            if(pushData(id,name,address) ){
+                System.out.println(" ADD Successfully !!!😁");
+            }
+            else {
+                System.out.println(" Something Wrong !!!🤔");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    protected boolean pushData (String id, String name, String address) throws ClassNotFoundException, SQLException {
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Javax", "root", "1234");
+        PreparedStatement statement = connection.prepareStatement("INSERT INTO student VALUES(?,?,?)");
+        statement.setString(1, id);
+        statement.setString(2, name);
+        statement.setString(3, address);
+        return statement.executeUpdate() > 0;
+
 
     }
 
